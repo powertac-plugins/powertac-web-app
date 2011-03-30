f469178b578099fff0a12ecdd06da5405e785065
ddauer <ddauer@gmail.com>
2011-03-30 21:10:15 +0200

    Correct genco dependency

--- PowertacWebAppGrailsPlugin.groovy
+++ PowertacWebAppGrailsPlugin.groovy
@@ -4,7 +4,8 @@ class PowertacWebAppGrailsPlugin {
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.3.7 > *"
   // the other plugins this plugin depends on
-  def dependsOn = ['powertacCommon': '0.9 > *']
+  def dependsOn = ['powertacCommon': '0.9 > *',
+                   'powertacSimpleGenco':'0.1 > *']
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
       //"grails-app/views/error.gsp"
