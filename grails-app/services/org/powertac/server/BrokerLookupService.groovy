88eef8d2514f13f4c7698dbae5a0ce83d3450308
Nguyen Nguyen <nguyen.a.nguyen@gmail.com>
2011-07-16 03:11:46 -0500

    Adding broker authentication management from predefined file

--- grails-app/services/org/powertac/server/BrokerLookupService.groovy
+++ grails-app/services/org/powertac/server/BrokerLookupService.groovy
@@ -11,20 +11,30 @@ class BrokerLookupService {
 
   static transactional = true
   def springSecurityService
+  def participantManagementService
 
   def findByLoginRequest(request) {
     log.debug("findByLoginRequest - start")
     def broker = Broker.findByUsername("${request.username}")
     if (!broker) {
       if (ConfigurationHolder.config?.powertac?.deployment?.type != 'competition') {
-        broker = new Broker(username: request.username.text(),
-                   password: springSecurityService.encodePassword(request.password.text()),
-                   enabled: true).save(failOnError: true)
+        broker = createNewBroker(request.username.text(), request.password.text())
+      } else {
+        if (participantManagementService.authenticate(request.username.text())) {
+          broker = createNewBroker(request.username.text(), request.password.text())
+        }
       }
     }
-    log.debug("broker is ${broker}, errors are ${broker.errors}")
+
+    log.debug("broker is ${broker}, errors are ${broker?.errors}")
     log.debug("findByLoginRequest - end")
 
     return broker
+  }
+
+  def createNewBroker(username, password) {
+    return new Broker(username: username,
+                   password: springSecurityService.encodePassword(password),
+                   enabled: true).save(failOnError: true)
   }
 }
---
+++ test/unit/org/powertac/server/ParticipantManagementServiceTests.groovy
@@ -0,0 +1,18 @@
+package org.powertac.server
+
+import grails.test.*
+
+class ParticipantManagementServiceTests extends GrailsUnitTestCase
+{
+  protected void setUp () {
+    super.setUp()
+  }
+
+  protected void tearDown () {
+    super.tearDown()
+  }
+
+  void testSomething () {
+
+  }
+}
